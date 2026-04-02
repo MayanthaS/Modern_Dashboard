f1ea16883111f24549c814265a2b0682f79aa256
MayanthaS <sapumalmayantha@gmail.com>
2026-04-02 17:53:45 +0530

    added styles of the sidebar css

--- src/layout/sidebar/sidebar.jsx
+++ src/layout/sidebar/sidebar.jsx
@@ -5,7 +5,7 @@ import { navigationLinks } from "../../data/data";
 
 const sidebar = () => {
   return (
-    <div className={`sidebar`}>
+    <div className={`sidebar `}>
       <div className="user-info">
         <div className="info-img img-fit-cover">
           <img src={personsImgs.person_two} alt="profile image" />
@@ -15,7 +15,7 @@ const sidebar = () => {
       <nav className="navigation">
         <ul className="nav-list">
           {navigationLinks.map((navigationLinks) => (
-            <li className="mav-items" key={navigationLinks.id}>
+            <li className="nav-items" key={navigationLinks.id}>
               <a href="#" className={`nav-link`}>
                 <img
                   src={navigationLinks.image}
