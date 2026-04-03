d2afa6e271b9815a7bbedece63f2667990bda550
MayanthaS <sapumalmayantha@gmail.com>
2026-04-03 19:33:30 +0530

    style: change transaction card to blue theme

--- src/component/ContentMain/ContainMain.jsx
+++ src/component/ContentMain/ContainMain.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Cards from "../../component/Cards/Cards";
+import Transaction from "../../Transaction/Transaction";
 import "./ContainMain.css";
 const ContainMain = () => {
   return (
     <div className="main-content-holder">
       <div className="content-grid-one">
         <Cards />
+        <Transaction />
       </div>
     </div>
   );
