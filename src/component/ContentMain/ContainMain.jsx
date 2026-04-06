495578835b06e9eebd8ecd1f5cef99c9fadc504f
MayanthaS <sapumalmayantha@gmail.com>
2026-04-06 13:40:04 +0530

    fix(report): restore chart JSX and resolve Vite parse error"

--- src/component/ContentMain/ContainMain.jsx
+++ src/component/ContentMain/ContainMain.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Cards from "../../component/Cards/Cards";
 import Transaction from "../../Transaction/Transaction";
 import "./ContainMain.css";
+import Report from "../../Report/Report";
 const ContainMain = () => {
   return (
     <div className="main-content-holder">
       <div className="content-grid-one">
         <Cards />
         <Transaction />
+        <Report />
       </div>
     </div>
   );
